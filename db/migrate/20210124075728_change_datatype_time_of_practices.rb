f7afb8c4e934276609d9e9ca79c0f3dcb70ca3a3
tsumutsumu <noritsumu0208@gmail.com>
2021-01-26 08:49:53 +0000

    modify

--- db/migrate/20210124075728_change_datatype_time_of_practices.rb
+++ db/migrate/20210124075728_change_datatype_time_of_practices.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeTimeOfPractices < ActiveRecord::Migration[5.2]
   def change
-    change_column :practices, :time, :integer
+    change_column :practices, :time, 'integer USING CAST(column_name AS integer)'
   end
 end
